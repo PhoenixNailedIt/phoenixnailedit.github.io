3a8c4690e8ffbfdd39117d679ceb86f459c2d4f9
PhoenixNailedIt <phoenixnailedit.biz@gmail.com>
2025-02-03 20:32:39 -0500

    2.6.10
    dude wtf is this gpt doing

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-// Falling Kitty Images: Ensure they fall from below the header and loop continuously
-window.onload = function() {
-  const fallingImages = document.querySelectorAll('.falling-image');
-  
-  fallingImages.forEach(image => {
-    image.style.animation = 'fall 5s infinite';
-  });
-};
+// Function to animate falling kitties
+function animateFallingKitties() {
+  // Create an array of images that will fall
+  const fallingKittyImages = [
+    "images/nerdy kitty.png",
+    "images/nerdy kitty.png",
+    "images/nerdy kitty.png",
+    "images/nerdy kitty.png",
+    "images/nerdy kitty.png",
+  ];
 
+  fallingKittyImages.forEach(imageSrc => {
+    // Create a new img element
+    const imgElement = document.createElement('img');
+    imgElement.src = imageSrc;
+    imgElement.alt = "Falling Kitty";
+    imgElement.classList.add('falling-image');
+
+    // Append the image to the body
+    document.body.appendChild(imgElement);
+
+    // Set animation for each image
+    imgElement.style.animation = 'fall 5s infinite';
+  });
+}
+
+// Start the falling kitties animation when the page loads
+window.onload = function() {
+  animateFallingKitties();
+};
 
